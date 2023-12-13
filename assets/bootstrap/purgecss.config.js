5ec6b4d71f065031b866237c336993878a74cec8
Yama56800 <thomawmahiaw@gmail.com>
2023-12-13 18:26:02 +0100

    73 commmit

---
+++ assets/bootstrap/purgecss.config.js
@@ -0,0 +1,6 @@
+// purgecss.config.js
+module.exports = {
+    content: ['././index.html', './assets/maugallery.js', '././index.html'], // Les fichiers à analyser
+    css: ['bootstrap.css'], // Les fichiers CSS à purger
+  };
+  
